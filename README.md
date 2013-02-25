6f24c1c4cbeed82406111579621de94a287bd3dd
Alessandro Andrioni <alessandroandrioni@gmail.com>
2013-02-24 23:16:39 -0300

    Add six new functions

--- README.md
+++ README.md
@@ -5,9 +5,15 @@ Catalan: a combinatorics library for Julia, focusing mostly (as of now) on enume
 Loading and using `bigint.jl` as arguments is extremely recommended, as these functions overflow easily.
 
 Implements the following functions, with yet no real regard for performance:
- - `catalan(n)`: returns the n-th Catalan number;
- - `derangement(n)`/`subfactorial(n)`: returns the number of permutations of n with no fixed points;
- - `fibonacci(n)`: the n-th Fibonacci number, uses precomputed table for `n` upto 92 (i.e. the largest Fibonacci number which fits in a `Int64`;
- - `hyperfactorial(n)`: the n-th hyperfactorial, i.e. prod([i^i for i = 2:n];
- - `multinomial(k...)`: receives a tuple of `k_1, ..., k_n` and calculates the multinomial coefficient `(n k)`, where `n = sum(k)`;
- - `stirlings1(n, k)`: the signed `(n,k)`-th Stirling number of the first kind.
+ - `catalan(n)`: returns the n-th Catalan number; always returns a `BigInt`;
+ - `derangement(n)`/`subfactorial(n)`: returns the number of permutations of n with no fixed points; always returns a `BigInt`;
+ - `doublefactorial(n)`: returns the double factorial n!!; always returns a `BigInt`;
+ - `fibonacci(n)`: the n-th Fibonacci number; always returns a `BigInt`;
+ - `hyperfactorial(n)`: the n-th hyperfactorial, i.e. prod([i^i for i = 2:n]; always returns a `BigInt`;
+ - `jacobisymbol(a,b)`: returns the Jacobi symbol (a/b);
+ - `legendresymbol(a,p)`: returns the Legendre symbol (a/p);
+ - `lucas(n)`: the n-th Lucas number; always returns a `BigInt`;
+ - `multifactorial(n)`: returns the m-multifactorial n(!^m); always returns a `BigInt`;
+ - `multinomial(k...)`: receives a tuple of `k_1, ..., k_n` and calculates the multinomial coefficient `(n k)`, where `n = sum(k)`; returns a `BigInt` only if given a `BigInt`;
+ - `primorial(n)`: returns the product of all positive prime numbers <= n; always returns a `BigInt`;
+ - `stirlings1(n, k)`: the signed `(n,k)`-th Stirling number of the first kind; returns a `BigInt` only if given a `BigInt`.
--- src/Catalan.jl
+++ src/Catalan.jl
@@ -1,10 +1,12 @@
+require("poly.jl")
+
 module Catalan
 
-import Base.*
-export catalan, derangement, fibonacci, hyperfactorial, multinomial, stirlings1, subfactorial
+import  Main.poly
 
-include("extras/bigint.jl")
-include("extras/poly.jl")
+export  catalan, derangement, fibonacci, hyperfactorial, multinomial,
+        stirlings1, subfactorial, lucas, primorial, doublefactorial,
+        multifactorial, legendresymbol, jacobisymbol
 
 # Multinomial coefficient where n = sum(k)
 function multinomial(k...)
@@ -18,7 +20,8 @@ function multinomial(k...)
 end
 
 # The number of permutations of n with no fixed points (subfactorial) 
-function derangement(n::Integer)
+function derangement(sn::Integer)
+    n = BigInt(sn)
     return num(factorial(n)*sum([(-1)^k//factorial(k) for k=0:n]))
 end
 subfactorial(n::Integer) = derangement(n)
@@ -30,7 +33,7 @@ function stirlings1(n::Integer, k::Integer)
 end
 
 # Hyperfactorial
-hyperfactorial(n::Integer) = prod([i^i for i = 2:n])
+hyperfactorial(n::Integer) = prod([i^i for i = BigInt(2):n])
 
 # Returns the n-th Catalan number
 function catalan(bn::Integer)
@@ -42,122 +45,68 @@ function catalan(bn::Integer)
     div(binomial(2*n, n), (n + 1))
 end
 
-function fibonacci(bn::BigInt)
-    z = _jl_bigint_init()
-    if bn<0
-        return BigInt(0)
-    else
-        n = uint(bn)
-    end
-    ccall(dlsym(_jl_libgmp_wrapper, :_jl_mpz_fib_ui), Void,
-        (Ptr{Void}, Uint), z, n)
-    BigInt(z)
-end
-
-# Overflows Int64 for n = 93+
-# Overflows Int32 for n = 47+
 function fibonacci(n::Integer)
-    if n > 92
-        error("Fibonacci over 92 overflows Int64, use the BigInt method")
-    elseif n < 0
-        return 0
-    else
-        return int64(FIBONACCI[n + 1])
+    if n < 0
+        throw(DomainError())
     end
+    z = BigInt()
+    ccall((:__gmpz_fib_ui, :libgmp), Void,
+        (Ptr{Void}, Uint), z.mpz, uint(n))
+    return z
 end
 
-const FIBONACCI = [ "0"
-                    "1"
-                    "1"
-                    "2"
-                    "3"
-                    "5"
-                    "8"
-                    "13"
-                    "21"
-                    "34"
-                    "55"
-                    "89"
-                    "144"
-                    "233"
-                    "377"
-                    "610"
-                    "987"
-                    "1597"
-                    "2584"
-                    "4181"
-                    "6765"
-                    "10946"
-                    "17711"
-                    "28657"
-                    "46368"
-                    "75025"
-                    "121393"
-                    "196418"
-                    "317811"
-                    "514229"
-                    "832040"
-                    "1346269"
-                    "2178309"
-                    "3524578"
-                    "5702887"
-                    "9227465"
-                    "14930352"
-                    "24157817"
-                    "39088169"
-                    "63245986"
-                    "102334155"
-                    "165580141"
-                    "267914296"
-                    "433494437"
-                    "701408733"
-                    "1134903170"
-                    "1836311903"
-                    "2971215073"
-                    "4807526976"
-                    "7778742049"
-                    "12586269025"
-                    "20365011074"
-                    "32951280099"
-                    "53316291173"
-                    "86267571272"
-                    "139583862445"
-                    "225851433717"
-                    "365435296162"
-                    "591286729879"
-                    "956722026041"
-                    "1548008755920"
-                    "2504730781961"
-                    "4052739537881"
-                    "6557470319842"
-                    "10610209857723"
-                    "17167680177565"
-                    "27777890035288"
-                    "44945570212853"
-                    "72723460248141"
-                    "117669030460994"
-                    "190392490709135"
-                    "308061521170129"
-                    "498454011879264"
-                    "806515533049393"
-                    "1304969544928657"
-                    "2111485077978050"
-                    "3416454622906707"
-                    "5527939700884757"
-                    "8944394323791464"
-                    "14472334024676221"
-                    "23416728348467685"
-                    "37889062373143906"
-                    "61305790721611591"
-                    "99194853094755497"
-                    "160500643816367088"
-                    "259695496911122585"
-                    "420196140727489673"
-                    "679891637638612258"
-                    "1100087778366101931"
-                    "1779979416004714189"
-                    "2880067194370816120"
-                    "4660046610375530309"
-                    "7540113804746346429"]
+function lucas(n::Integer)
+    if n < 0
+        throw(DomainError())
+    end
+    z = BigInt()
+    ccall((:__gmpz_lucnum_ui, :libgmp), Void,
+        (Ptr{Void}, Uint), z.mpz, uint(n))
+    return z
+end
+
+function primorial(n::Integer)
+    if n < 0
+        throw(DomainError())
+    end
+    z = BigInt()
+    ccall((:__gmpz_primorial_ui, :libgmp), Void,
+        (Ptr{Void}, Uint), z.mpz, uint(n))
+    return z
+end
+
+function doublefactorial(n::Integer)
+    if n < 0
+        throw(DomainError())
+    end
+    z = BigInt()
+    ccall((:__gmpz_2fac_ui, :libgmp), Void,
+        (Ptr{Void}, Uint), z.mpz, uint(n))
+    return z
+end
+
+function multifactorial(n::Integer, m::Integer)
+    if n < 0
+        throw(DomainError())
+    end
+    z = BigInt()
+    ccall((:__gmpz_mfac_uiui, :libgmp), Void,
+        (Ptr{Void}, Uint, Uint), z.mpz, uint(n), uint(m))
+    return z
+end
+
+function legendresymbol(a::Integer, b::Integer)
+    ba = BigInt(a)
+    bb = BigInt(b)
+    return ccall((:__gmpz_legendre, :libgmp), Int,
+        (Ptr{Void}, Ptr{Void}), ba.mpz, bb.mpz)
+end
+
+function jacobisymbol(a::Integer, b::Integer)
+    ba = BigInt(a)
+    bb = BigInt(b)
+    return ccall((:__gmpz_jacobi, :libgmp), Int,
+        (Ptr{Void}, Ptr{Void}), ba.mpz, bb.mpz)
+end
 
 end
