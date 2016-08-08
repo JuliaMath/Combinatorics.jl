56a062b43db1876789cf530b3aa67cf62f2d18a3
Tamas Nagy <tamas@tamasnagy.com>
2016-08-08 15:23:20 -0700

    check if Julia version is post-combinatorics removal
    fixes #26

--- src/Combinatorics.jl
+++ src/Combinatorics.jl
@@ -8,8 +8,8 @@ import Base: start, next, done, length, eltype
 
 #These 8 functions were removed from Julia 0.5 as part of JuliaLang/julia#13897,
 #so check if it's necessary to import them to overload the stub methods left in
-#Base.
-if isdefined(Base, :combinations)
+#Base. Only do this if on a version of Julia post-combinatorics removal.
+if isdefined(Base, :combinations) && VERSION >= v"0.5.0-dev+1204"
     import Base: combinations, partitions, prevprod, levicivita, nthperm,
                  nthperm!, parity, permutations
 end
