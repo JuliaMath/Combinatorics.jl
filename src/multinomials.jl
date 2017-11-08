770f00886f6fac8f789c5245f36270d7b0ff03bb
Júlio Hoffimann <julio.hoffimann@gmail.com>
2017-11-07 23:53:49 -0800

    Implement iterator interface

--- src/multinomials.jl
+++ src/multinomials.jl
@@ -3,6 +3,35 @@
 
 export multiexponents
 
+struct MultiExponents{T}
+  c::Combinations{T}
+  nterms::Int
+end
+
+# transform combination into multinomial exponent
+function comb2exp(stars, nterms)
+    # stars minus their consecutive
+    # position becomes their index
+    idx = stars - [0:length(stars)-1;]
+
+    result = zeros(Int, nterms)
+    for i in idx
+      result[i] += 1
+    end
+
+    result
+end
+
+start(m::MultiExponents) = start(m.c)
+function next(m::MultiExponents, s)
+    item, ss = next(m.c, s)
+
+    comb2exp(item, m.nterms), ss
+end
+done(m::MultiExponents, s) = done(m.c, s)
+
+length(m::MultiExponents) = length(m.c)
+
 """
     multiexponents(m, n)
 
@@ -11,31 +40,19 @@ Returns the exponents in the multinomial expansion (x₁ + x₂ + ... + xₘ)ⁿ
 For example, the expansion (x₁ + x₂ + x₃)² = x₁² + x₁x₂ + x₁x₃ + ...
 has the exponents:
 
-    julia> multiexponents(3, 2)
+    julia> collect(multiexponents(3, 2))
 
-    6x3 Array{Int64,2}:
-     2  0  0
-     1  1  0
-     1  0  1
-     0  2  0
-     0  1  1
-     0  0  2
+    6-element Array{Any,1}:
+     [2, 0, 0]
+     [1, 1, 0]
+     [1, 0, 1]
+     [0, 2, 0]
+     [0, 1, 1]
+     [0, 0, 2]
 """
 function multiexponents(m, n)
-    # standard stars and bars
-    nsymbols = m+n-1
-    stars = combinations(1:nsymbols, n)
+    # standard stars and bars (nsymbols = m+n-1)
+    c = combinations(1:m+n-1, n)
 
-    N = length(stars)
-    result = zeros(Int, N, m)
-    for (i,star) in enumerate(stars)
-        # stars minus their consecutive
-        # position becomes their index
-        idx = star - [0:n-1;]
-        for j in idx
-            result[i,j] += 1
-        end
-    end
-
-    result
+    MultiExponents(c, m)
 end
