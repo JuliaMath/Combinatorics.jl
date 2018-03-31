efa9d0a0df698e192b92c86f5cc4488c50e07bea
Alex Arslan <ararslan@comcast.net>
2018-03-30 19:41:19 -0700

    Add powerset function (#63)
    Fixes #11.

--- test/combinations.jl
+++ test/combinations.jl
@@ -39,3 +39,9 @@ using Compat.Test
 @test_throws DomainError [CoolLexCombinations(5, 0)...]
 @test [CoolLexCombinations(4,2)...] == Vector[[1,2], [2,3], [1,3], [2,4], [3,4], [1,4]]
 @test isa(start(CoolLexCombinations(1000, 20)), Combinatorics.CoolLexIterState{BigInt})
+
+# Power set
+@test collect(powerset([])) == Any[[]]
+@test collect(powerset(['a', 'b', 'c'])) == Any[[],['a'],['b'],['c'],['a','b'],['a','c'],['b','c'],['a','b','c']]
+@test collect(powerset(['a', 'b', 'c'], 1)) == Any[['a'],['b'],['c'],['a','b'],['a','c'],['b','c'],['a','b','c']]
+@test collect(powerset(['a', 'b', 'c'], 1, 2)) == Any[['a'],['b'],['c'],['a','b'],['a','c'],['b','c']]
