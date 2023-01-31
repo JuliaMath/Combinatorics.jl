1bca9914930ae73bd8f6cd4fc1958dc93ef14e5c
Nate McIntosh <40805156+natemcintosh@users.noreply.github.com>
2023-01-31 13:04:04 -0500

    Update src/permutations.jl
    Co-authored-by: Alex Arslan <ararslan@comcast.net>

--- src/permutations.jl
+++ src/permutations.jl
@@ -18,7 +18,7 @@ function has_repeats(state::Vector{Int})
     # This can be safely marked inbounds because of the type restriction in the signature.
     # If the type restriction is ever loosened, please check safety of the `@inbounds`
     @inbounds for outer in eachindex(state)
-        @inbounds for inner in (outer+1):lastindex(state)
+        for inner in (outer+1):lastindex(state)
             if state[outer] == state[inner]
                 return true
             end
