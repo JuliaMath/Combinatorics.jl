8a7a8accb9f46a546c690fa3a27db00dfc724647
Nate McIntosh <40805156+natemcintosh@users.noreply.github.com>
2023-02-01 11:21:18 -0500

    faster permutations, based on what rdeits suggested (#122)
    * update permutations to what rdeits suggested
    
    also add testsets
    
    * remove old code
    
    * add suggestions from bkamins
    
    * add offset arrays for testing purposes only
    
    * add necessary libs to test dependencies
    
    * Added tests for offsetarrays
    
    attempt at fix does not work yet
    
    * Apply suggestions from code review
    
    Thanks @bkamins for the help on this!
    
    Co-authored-by: Bogumił Kamiński <bkamins@sgh.waw.pl>
    
    * all tests now passing
    
    * apply whitespace suggestions
    
    * try to make julia 1.0 happy
    
    * Add OffsetArrays as a test dependency
    
    Julia 1.0/1.1 syntax. See https://pkgdocs.julialang.org/v1/creating-packages/#Test-specific-dependencies-in-Julia-1.2-and-above
    
    * Apply suggestions from code review
    
    Co-authored-by: Bogumił Kamiński <bkamins@sgh.waw.pl>
    
    * everything seems to be working now
    
    * Return empty vector for invalid t args
    
    * special case for t=1
    
    * updates to reduce size of type union
    
    * A few more explanatory comments
    
    * Add suggestions
    
    from palday and jishnub
    
    * Change name of iterator to match convention
    
    * Update src/permutations.jl
    
    Co-authored-by: Alex Arslan <ararslan@comcast.net>
    
    * Update src/permutations.jl
    
    Co-authored-by: Alex Arslan <ararslan@comcast.net>
    
    * Update src/permutations.jl
    
    Co-authored-by: Alex Arslan <ararslan@comcast.net>
    
    * Update src/permutations.jl
    
    Co-authored-by: Alex Arslan <ararslan@comcast.net>
    
    * fix tests, and don't use Big
    
    ---------
    
    Co-authored-by: Bogumił Kamiński <bkamins@sgh.waw.pl>
    Co-authored-by: Alex Arslan <ararslan@comcast.net>

--- test/partitions.jl
+++ test/partitions.jl
@@ -1,45 +1,59 @@
-@test collect(partitions(4)) ==  Any[[4], [3,1], [2,2], [2,1,1], [1,1,1,1]]
-@test collect(partitions(8,3)) == Any[[6,1,1], [5,2,1], [4,3,1], [4,2,2], [3,3,2]]
-@test collect(partitions(8, 1)) == Any[[8]]
-@test collect(partitions(8, 9)) == []
-@test collect(partitions([1,2,3])) == Any[Any[[1,2,3]], Any[[1,2],[3]], Any[[1,3],[2]], Any[[1],[2,3]], Any[[1],[2],[3]]]
-@test collect(partitions([1,2,3,4],3)) == Any[Any[[1,2],[3],[4]], Any[[1,3],[2],[4]], Any[[1],[2,3],[4]],
-                                              Any[[1,4],[2],[3]], Any[[1],[2,4],[3]], Any[[1],[2],[3,4]]]
-@test collect(partitions([1,2,3,4],1)) == Any[Any[[1, 2, 3, 4]]]
-@test collect(partitions([1,2,3,4],5)) == []
+@testset "partitions" begin
+    @test collect(partitions(4)) == Any[[4], [3, 1], [2, 2], [2, 1, 1], [1, 1, 1, 1]]
+    @test collect(partitions(8, 3)) == Any[[6, 1, 1], [5, 2, 1], [4, 3, 1], [4, 2, 2], [3, 3, 2]]
+    @test collect(partitions(8, 1)) == Any[[8]]
+    @test collect(partitions(8, 9)) == []
+    @test collect(partitions([1, 2, 3])) == Any[Any[[1, 2, 3]], Any[[1, 2], [3]], Any[[1, 3], [2]], Any[[1], [2, 3]], Any[[1], [2], [3]]]
+    @test collect(partitions([1, 2, 3, 4], 3)) == Any[Any[[1, 2], [3], [4]], Any[[1, 3], [2], [4]], Any[[1], [2, 3], [4]],
+        Any[[1, 4], [2], [3]], Any[[1], [2, 4], [3]], Any[[1], [2], [3, 4]]]
+    @test collect(partitions([1, 2, 3, 4], 1)) == Any[Any[[1, 2, 3, 4]]]
+    @test collect(partitions([1, 2, 3, 4], 5)) == []
 
-@inferred first(partitions(4))
-@inferred first(partitions(8,3))
-@inferred first(partitions([1,2,3]))
-@inferred first(partitions([1,2,3,4],3))
+    @inferred first(partitions(4))
+    @inferred first(partitions(8, 3))
+    @inferred first(partitions([1, 2, 3]))
+    @inferred first(partitions([1, 2, 3, 4], 3))
 
-@test isa(collect(partitions(4)), Vector{Vector{Int}})
-@test isa(collect(partitions(8,3)), Vector{Vector{Int}})
-@test isa(collect(partitions([1,2,3])), Vector{Vector{Vector{Int}}})
-@test isa(collect(partitions([1,2,3,4], 3)), Vector{Vector{Vector{Int}}})
+    @test isa(collect(partitions(4)), Vector{Vector{Int}})
+    @test isa(collect(partitions(8, 3)), Vector{Vector{Int}})
+    @test isa(collect(partitions([1, 2, 3])), Vector{Vector{Vector{Int}}})
+    @test isa(collect(partitions([1, 2, 3, 4], 3)), Vector{Vector{Vector{Int}}})
 
-@test length(partitions(0)) == 1
-@test length(partitions(-1)) == 0
-@test length(collect(partitions(30))) == length(partitions(30))
-@test length(collect(partitions(90,4))) == length(partitions(90,4))
-@test length(collect(partitions('a':'h'))) == length(partitions('a':'h'))
-@test length(collect(partitions('a':'h',5))) == length(partitions('a':'h',5))
+    @test length(partitions(0)) == 1
+    @test length(partitions(-1)) == 0
+    @test length(collect(partitions(30))) == length(partitions(30))
+    @test length(collect(partitions(90, 4))) == length(partitions(90, 4))
+    @test length(collect(partitions('a':'h'))) == length(partitions('a':'h'))
+    @test length(collect(partitions('a':'h', 5))) == length(partitions('a':'h', 5))
 
-# integer_partitions
-@test integer_partitions(0) == []
-@test integer_partitions(5) == Any[[1, 1, 1, 1, 1], [2, 1, 1, 1], [2, 2, 1], [3, 1, 1], [3, 2], [4, 1], [5]]
-@test_throws DomainError integer_partitions(-1)
+    # integer_partitions
+    @test integer_partitions(0) == []
+    @test integer_partitions(5) == Any[[1, 1, 1, 1, 1], [2, 1, 1, 1], [2, 2, 1], [3, 1, 1], [3, 2], [4, 1], [5]]
+    @test_throws DomainError integer_partitions(-1)
 
-@test_throws ArgumentError prevprod([2,3,5],Int128(typemax(Int))+1)
-@test prevprod([2,3,5],30) == 30
-@test prevprod([2,3,5],33) == 32
+    @test_throws ArgumentError prevprod([2, 3, 5], Int128(typemax(Int)) + 1)
+    @test prevprod([2, 3, 5], 30) == 30
+    @test prevprod([2, 3, 5], 33) == 32
 
-# noncrossing partitions
-let nc4 = ncpartitions(4)
-    @test nc4 == Any[Any[[1],[2],[3],[4]], Any[[1],[2],[3,4]], Any[[1],[2,3],[4]], Any[[1],[2,4],[3]], Any[[1],[2,3,4]],
-                     Any[[1,2],[3],[4]], Any[[1,2],[3,4]],
-                     Any[[1,3],[2],[4]], Any[[1,4],[2],[3]], Any[[1,4],[2,3]],
-                     Any[[1,2,3],[4]], Any[[1,3,4],[2]], Any[[1,2,4],[3]],
-                     Any[[1,2,3,4]]]
-    @test length(nc4) == catalannum(4)
+    # noncrossing partitions
+    let nc4 = ncpartitions(4)
+        @test nc4 == Any[
+            Any[[1], [2], [3], [4]],
+            Any[[1], [2], [3, 4]],
+            Any[[1], [2, 3], [4]],
+            Any[[1], [2, 4], [3]],
+            Any[[1], [2, 3, 4]],
+            Any[[1, 2], [3], [4]],
+            Any[[1, 2], [3, 4]],
+            Any[[1, 3], [2], [4]],
+            Any[[1, 4], [2], [3]],
+            Any[[1, 4], [2, 3]],
+            Any[[1, 2, 3], [4]],
+            Any[[1, 3, 4], [2]],
+            Any[[1, 2, 4], [3]],
+            Any[[1, 2, 3, 4]],
+        ]
+        @test length(nc4) == catalannum(4)
+    end
+
 end
--- test/youngdiagrams.jl
+++ test/youngdiagrams.jl
@@ -1,19 +1,22 @@
-let λ = Partition([5,4,2,2]), μ = Partition([2,1])
-    @test λ \ μ == SkewDiagram(λ, μ)
-    @test leglength(λ, μ) == leglength(λ \ μ) == 3
-end
-@test leglength(Partition(Int[]), Partition(Int[])) == -1
+@testset "youngdiagrams" begin
+    let λ = Partition([5, 4, 2, 2]), μ = Partition([2, 1])
+        @test λ \ μ == SkewDiagram(λ, μ)
+        @test leglength(λ, μ) == leglength(λ \ μ) == 3
+    end
+    @test leglength(Partition(Int[]), Partition(Int[])) == -1
 
-@test isrimhook(Partition([4,3,2]), Partition([2,2,2]))
-@test !isrimhook(Partition([4,3,2]), Partition([2,2,1]))
-@test !isrimhook(Partition([4,3,2]), Partition([1,1]))
+    @test isrimhook(Partition([4, 3, 2]), Partition([2, 2, 2]))
+    @test !isrimhook(Partition([4, 3, 2]), Partition([2, 2, 1]))
+    @test !isrimhook(Partition([4, 3, 2]), Partition([1, 1]))
 
-let λ = Partition([5,4,2,1]), μ = Partition([4,3,2,2,1])
-    @test partitionsequence(λ) == [1, 0, 1, 0, 1, 1, 0, 1, 0]
-    @test character(λ, μ) == 0
-end
+    let λ = Partition([5, 4, 2, 1]), μ = Partition([4, 3, 2, 2, 1])
+        @test partitionsequence(λ) == [1, 0, 1, 0, 1, 1, 0, 1, 0]
+        @test character(λ, μ) == 0
+    end
 
-let λ = μ = Partition([1])
-    @test leglength(λ, μ) == -1
-    @test character(λ, μ) == 1
+    let λ = μ = Partition([1])
+        @test leglength(λ, μ) == -1
+        @test character(λ, μ) == 1
+    end
+
 end
