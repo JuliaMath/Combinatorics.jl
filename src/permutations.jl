684401f55b0d54c4718d074a665111fbec3d7497
Chengyu HAN <cyhan.dev@outlook.com>
2025-01-01 15:56:45 +0800

    doc: add examples for `nthperm(a, k)`

--- src/permutations.jl
+++ src/permutations.jl
@@ -288,6 +288,28 @@ end
     nthperm(a, k)
 
 Compute the `k`th lexicographic permutation of the vector `a`.
+
+# Examples
+```jldoctest
+julia> collect(permutations([1,2]))
+2-element Vector{Vector{Int64}}:
+ [1, 2]
+ [2, 1]
+
+julia> nthperm([1,2], 1)
+2-element Vector{Int64}:
+ 1
+ 2
+
+julia> nthperm([1,2], 2)
+2-element Vector{Int64}:
+ 2
+ 1
+
+julia> nthperm([1,2], 3)
+ERROR: ArgumentError: permutation k must satisfy 0 < k ≤ 2, got 3
+[...]
+```
 """
 nthperm(a::AbstractVector, k::Integer) = nthperm!(collect(a), k)
 
