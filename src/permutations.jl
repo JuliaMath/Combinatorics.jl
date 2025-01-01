4b8efab4a4c694f8ec20c306cbdfc46c00e4eb37
Chengyu HAN <cyhan.dev@outlook.com>
2025-01-01 15:47:31 +0800

    doc: add examples for `parity(p)`

--- src/permutations.jl
+++ src/permutations.jl
@@ -379,6 +379,22 @@ end
 Compute the parity of a permutation `p` using the [`levicivita`](@ref) function,
 permitting calls such as `iseven(parity(p))`. If `p` is not a permutation then an
 error is thrown.
+
+# Examples
+```jldoctest
+julia> parity([1, 2, 3])
+0
+
+julia> parity([3, 2, 1])
+1
+
+julia> parity([1, 1, 1])
+ERROR: ArgumentError: Not a permutation
+[...]
+
+julia> parity((collect(1:100)))
+0
+```
 """
 function parity(p::AbstractVector{<:Integer})
     epsilon = levicivita(p)
