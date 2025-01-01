380a90723d9fd202b58616e3d9a6f5bbc4b3f5af
Chengyu HAN <cyhan.dev@outlook.com>
2025-01-01 15:22:01 +0800

    doc: add examples for `multiset_permutations(a, t)`

--- src/permutations.jl
+++ src/permutations.jl
@@ -215,6 +215,28 @@ end
     multiset_permutations(a, t)
 
 Generate all permutations of size `t` from an array `a` with possibly duplicated elements.
+
+# Examples
+```jldoctest
+julia> collect(permutations([1,1,1], 2))
+6-element Vector{Vector{Int64}}:
+ [1, 1]
+ [1, 1]
+ [1, 1]
+ [1, 1]
+ [1, 1]
+ [1, 1]
+
+julia> collect(multiset_permutations([1,1,1], 2))
+1-element Vector{Vector{Int64}}:
+ [1, 1]
+
+julia> collect(multiset_permutations([1,1,2], 3))
+3-element Vector{Vector{Int64}}:
+ [1, 1, 2]
+ [1, 2, 1]
+ [2, 1, 1]
+```
 """
 function multiset_permutations(a, t::Integer)
     m = unique(collect(a))
