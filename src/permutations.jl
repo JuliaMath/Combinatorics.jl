d146a67d4dc7ebcbf282b700bdca49e6667251e8
Chengyu HAN <cyhan.dev@outlook.com>
2025-01-01 15:42:24 +0800

    doc: add examples for `levicivita(p)`

--- src/permutations.jl
+++ src/permutations.jl
@@ -325,6 +325,24 @@ is even, -1 if it is odd, and 0 otherwise.
 
 The parity is computed by using the fact that a permutation is odd if and
 only if the number of even-length cycles is odd.
+
+# Examples
+```jldoctest
+julia> levicivita([1, 2, 3])
+1
+
+julia> levicivita([3, 2, 1])
+-1
+
+julia> levicivita([1, 1, 1])
+0
+
+julia> levicivita(collect(1:100))
+1
+
+julia> levicivita(ones(Int, 100))
+0
+````
 """
 function levicivita(p::AbstractVector{<:Integer})
     n = length(p)
